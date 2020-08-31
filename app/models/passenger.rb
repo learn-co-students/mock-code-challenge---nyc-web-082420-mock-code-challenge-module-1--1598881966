c5f3d7b478b1ab8b09fb08bbc82a37de03ab3008
jakeFromm <jacobfromm@gmail.com>
2020-08-31 11:10:19 -0400

    deliverables deleted from class files

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -30,18 +30,3 @@ class Passenger
     end
  
 end
-
-# #### Passenger
-# A Passenger should be initialized with a name as a string. After the Passenger has been initialized, it shouldn't be changed.
-# - `Passenger#name` DONE
-#   - Returns the name of the passenger
-# - `Passenger#rides` DONE
-#   - Returns an array of Ride instances that this person has been on
-# - `Passenger#drivers` DONE
-#   - Returns an array of Driver instances that this person has rode with
-# - `Passenger#total_distance` DONE
-#   - Returns the floating number that represents the total distance the passenger has travelled using the service
-# - `Passenger.all` DONE
-#   - Returns an array of all Passengers
-# - `Passenger.premium_members` DONE
-#   - Returns an array of all Passengers who have travelled over 100 miles in total with the service
