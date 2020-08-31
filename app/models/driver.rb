e6496a872e4b84ac7a3ae8858c2d547cabb1f5f9
laflamablanc <sean.laflam@gmail.com>
2020-08-31 11:02:38 -0400

    Driver deliverables complete

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,15 +1,39 @@
 class Driver
 
-  attr_accessor :name
+  attr_reader :name
   @@all = []
 
   def initialize(name)
     @name = name
-    @@all = []
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def rides
+    Ride.all.select {|ride| ride.driver == self}
+  end
+
+  def passenger_names
+    passenger_list = rides.map {|ride| ride.passenger}
+    passenger_list.uniq
+  end
+
+  def self.mileage_cap(distance)
+    #assuming this is total miles driven, not a single ride cap
+    Driver.all.select do |driver|
+      driver.total_distance_driven > distance
+    end
+  end
+
+  def total_distance_driven
+    total = 0
+    rides.each do |ride|
+      total += ride.distance
+    end
+    total
   end
 
 end
