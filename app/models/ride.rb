f706f5fe50fd7e4df44ca82178d38c3bf3537188
KingFisher15 <gordond227@gmail.com>
2020-08-31 11:35:16 -0400

    attempt

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,12 @@
+class Ride
+    @@all= []
 
+def initalize (driver, passenger)
+    @driver = driver
+    @passenger = passenger
+    @@all << self
+end 
+
+def passenger
+    Passenger.all.select {|P| p.passenger == self}
+end 
