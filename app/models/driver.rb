41645399ce41bece5c97792cc0d310c112ce7d66
jakeFromm <jacobfromm@gmail.com>
2020-08-31 11:05:42 -0400

    all deliverables done for all classes

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -11,8 +11,8 @@ class Driver
         @@all
     end
 
-    def self.mileage_cap(distance)
-
+    def self.mileage_cap(cap)
+        Driver.all.select {|drive| drive.total_distance > cap}
     end
 
     def rides
@@ -22,18 +22,24 @@ class Driver
     def passenger_names
         self.rides.map {|ride| ride.passenger.name}.uniq
     end
+
+    def total_distance
+        dist = 0
+        self.rides.each {|driven| dist += driven.distance}
+        dist
+    end
 end
 
 # #### Driver
 # A Driver should be initialized with a name as a string.
-# - `Driver#name`
+# - `Driver#name` DONE
 #   - Returns the driver's name
-# - `Driver#passenger_names`
+# - `Driver#passenger_names` DONE
 #   - Returns an array of all Passengers' names a driver has driven. The names should be **unique** (no repeats).
-# - `Driver#rides`
+# - `Driver#rides` DONE
 #   - Returns an array of all Rides a driver has made
-# - `Driver.all`
+# - `Driver.all` DONE
 #   - Returns an array of all Drivers
-# - `Driver.mileage_cap(distance)`
+# - `Driver.mileage_cap(distance)` DONE
 #   - Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
 
