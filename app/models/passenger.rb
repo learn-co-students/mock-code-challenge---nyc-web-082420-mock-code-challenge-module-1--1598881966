fc3823755f15dc64b27a3ce9a8f93d726127b7e0
aimee-liang <aimeeeliang@gmail.com>
2020-08-31 11:09:33 -0400

    Sixth commit - edits to passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -22,8 +22,15 @@ class Passenger
     Ride.all.select{|driver| driver.passenger == self}
 
     # total distance method returns floating number (total distance) passenger has travelled
+    # iterate through ride.all and for each time passenger has ridden, increase and eturn total distance
     def total_distance
-        Ride.all.
+        total = 0 #will be incrementing as we iterate through array
+        Ride.all.each do |passenger| #iterate through array
+            if passenger.name == self #set conditional so only looking at instance of self
+            passenger.distance += total #review syntax of to_f
+            end
+        end
+        total.to_f
     end
 
     # class method all
@@ -34,6 +41,7 @@ class Passenger
     # class method premium members is an array of all passengers who travelled more than 100 mi
     def self.premium_members
         # need to invoke average distance?
+
     end
 
 end
