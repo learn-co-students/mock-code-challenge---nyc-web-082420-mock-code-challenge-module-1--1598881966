89ec257e2d4ad68e3f9db16aaa301f3e1e53d195
gabrielhicks <gabrielhicks@mail.com>
2020-08-31 10:53:45 -0400

    finished passenger methods

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,37 +4,38 @@ class Driver
     @@all = []
 
     def initialize(name)
-# - `Driver#name`
+        # - `Driver#name`
         @name = name
         @@all << self
     end
 
     def self.all
-# - `Driver.all`
+        # - `Driver.all`
         @@all
     end
 
     def passenger_names
-# - `Driver#passenger_names`
-#   - Returns an array of all Passengers' names a driver has driven. The names should be **unique** (no repeats).
+        # - `Driver#passenger_names`
+        #   - Returns an array of all Passengers' names a driver has driven. The names should be **unique** (no repeats).
         Ride.all.select {|ride| ride.driver == self}.map {|ride| ride.passenger.name}.uniq
     end
 
     def rides
-# - `Driver#rides`
-#   - Returns an array of all Rides a driver has made
+        # - `Driver#rides`
+        #   - Returns an array of all Rides a driver has made
         Ride.all.select {|ride| ride.driver == self}
     end
 
     def distance_total
-# helper for mileage_cap
+        # helper for mileage_cap
         Ride.all.select {|ride| ride.driver == self}.map {|ride| ride.distance}.sum
     end
 
     def self.mileage_cap(distance)
-# - `Driver.mileage_cap(distance)`
-#   - Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
+        # - `Driver.mileage_cap(distance)`
+        #   - Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
         total = Ride.all.select {|ride| ride.driver}.map {|ride| ride.driver.distance_total}
+        # I think in only need this second code. Unsure if I could use variables to lessen the time issue of .map.find_all.uniq
         each_driver = Driver.all.select {|driver| driver.distance_total}.find_all {|driver| driver.distance_total > distance}
     end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,36 +4,40 @@ class Passenger
     @@all = []
 
     def initialize(name)
-# should be initialized with a name as a string. After the Passenger has been initialized, it shouldn't be changed.
-# - `Passenger#name`
+        # should be initialized with a name as a string. After the Passenger has been initialized, it shouldn't be changed.
+        # - `Passenger#name`
         @name = name
         @@all << self
     end
 
     def self.all
-#   - Returns an array of all Passengers
+        #   - Returns an array of all Passengers
         @@all
     end
 
     def rides
-# array of all rides ive(instance) been on
-
+        # array of all rides ive(instance) been on
+        Ride.all.select {|ride| ride.passenger == self}
     end
 
     def drivers
-# array of all driers ive(instance) had
-
+        # array of all driers ive(instance) had
+        self.rides.map {|ride| ride.driver}.uniq
+        # with names returned as strings in array
+        # jake.rides.map {|ride| ride.driver.name}.uniq
     end
 
     def total_distance
-# total distance of my rides
-
+        # total distance of my rides
+        Ride.all.select {|ride| ride.passenger == self}.map {|ride| ride.distance}.sum
     end
 
     def self.premium_members
-# - `Passenger.premium_members`
-#   - Returns an array of all Passengers who have travelled over 100 miles in total with the service
-
+        # - `Passenger.premium_members`
+        #   - Returns an array of all Passengers who have travelled over 100 miles in total with the service
+        total = Ride.all.map {|ride| ride.passenger}.map {|ride| ride.total_distance}.uniq
+        # I think in only need this second code. Unsure if I could use variables to lessen the time issue of .map.find_all.uniq
+        each_passenger = Ride.all.map {|ride| ride.passenger}.find_all {|passenger| passenger.total_distance > 100}.uniq
     end
     
 end
