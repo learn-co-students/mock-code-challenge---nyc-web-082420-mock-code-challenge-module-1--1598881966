1f72aa4ee5f5beae8603ba2c8ec49c62ffa09104
aimee-liang <aimeeeliang@gmail.com>
2020-08-31 10:17:48 -0400

    Ride deliverables

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,36 @@
 
+class Ride
+
+    # need attrs
+    attr_accessor :driver, :passenger, :distance 
+    # class variable all
+    @@all = []
+
+    # initialize with driver, passenger, distance (as float) in miles
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+        @@all << self
+    end
+
+    # passenger method returns passenger for that ride
+    def passenger
+        @passenger
+    end
+
+    # driver method returns driver for that ride
+    def driver
+        @driver
+    end
+
+    # distance method returns distance for ride
+    def distance
+        @distance.to_f  #need to check on float method
+    end
+
+    #class method average distance for all rides
+    def self.average_distance
+        self.all.distance.inject {|sum, el| sum + el}.to_f / self.all.distance.size
+    end
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment.rb'
 
+require 'pry'
 def reload
   load 'config/environment.rb'
 end
