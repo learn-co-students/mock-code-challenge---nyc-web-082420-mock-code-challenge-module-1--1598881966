29a185fd9fa4590be147d79cc23b34a74a86f178
aimee-liang <aimeeeliang@gmail.com>
2020-08-31 10:34:24 -0400

    Driver deliverables(some)

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,39 @@
+class Driver
+    # attrs
+    attr_accessor :name
 
+    # class variable all 
+    @@all = []
+
+    # should be initialized with name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    #name method returns name
+    def name
+        @name
+    end
+
+    # REVIEW - passenger names method returns array of all Passenger names driver has driven. Should be uniq
+    def passenger_names
+        Ride.all.select {}
+    end
+
+    # REVIEW - drives method returns array of all rides driver has made
+    def rides
+        Ride.all.filter {|rides| rides.name == self}
+    end
+
+    # class method all should return all Drivers
+    def self.all
+        @@all
+    end
+
+    # mileage_cap w/arg of distance returns array of all Drivers who drove over distance
+    def self.mileage_cap(distance)
+        Ride.all.filter{}
+    end
+    
+end
