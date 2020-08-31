a944d4bcde05450e641975e9ac79f6ed3d660359
aimee-liang <aimeeeliang@gmail.com>
2020-08-31 11:26:36 -0400

    Eighth commit - working on console.rb

--- tools/console.rb
+++ tools/console.rb
@@ -10,7 +10,24 @@ end
 
 # Drivers
 
+anna = Driver.new("Anna")
+billy = Driver.new("Billy")
+carl = Driver.new("Carl")
+
 # Passengers
+
+bob = Passenger.new("Bob")
+cathy = Passenger.new("Cathy")
+dani = Passenger.new("Dani")
+
+# Rides
+
+Ride_1 = Ride.new(anna, bob, 50)
+Ride_2 = Ride.new(billy, cathy, 100)
+Ride_3 = Ride.new(carl, dani, 150)
+Ride_4 = Ride.new(anna, dani, 200)
+
+
 
 
 binding.pry
