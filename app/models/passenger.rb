95b528b995e721fdb00f7dbf45167780566c26bf
chlorox416 <chloechang416@gmail.com>
2020-08-31 10:35:56 -0400

    save

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,35 @@
+class Passenger
 
+attr_reader :name
+@@all = []
+
+def initalize(name)
+    @name = name
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def name
+    puts "Passenger name is #{@name}."
+end
+
+def rides
+end
+
+def drivers
+end
+
+def total_distance
+end
+
+def self.all
+end
+
+def self.premium_memebers
+
+
+
+end   
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,32 @@
+class Ride
 
+attr_accessor :driver, :passenger, :distance
+@@all = []
+def initialize(driver, passenger, distance)
+    @driver = driver
+    @passenger = passenger
+    @distance = distance
+    @@all << self
+end
+
+def self.all
+    @@self
+end
+
+def ride_passenger
+end
+
+def ride_driver
+end
+
+def distance
+end
+
+def self.average_distance
+end
+
+
+
+
+
+end    
