f74bb76893c40235c3f4ca9f1104a9a1838981ad
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-08-31 10:32:12 -0400

    Finished Passenger and Driver class outlines

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,42 @@
+class Driver 
+    attr_reader :name 
+    @@all = [] 
 
+    def initialized(name)
+        @name = name 
+        @@all << self 
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def passenger_names
+
+    end
+
+    def rides 
+        Ride.all.select { |ride| ride == self}
+    end
+
+    def self.mileage_cap(distance)
+    end
+
+
+
+end
+
+
+
+
+
+
+
+
+# #### Driver
+
+# - `Driver#passenger_names`
+#   - Returns an array of all Passengers' names a driver has driven. The names should be **unique** (no repeats).
+
+# - `Driver.mileage_cap(distance)`
+#   - Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,40 @@
+class Passenger 
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self  
+    end
+
+    def rides 
+        Ride.all.select { |ride| ride == self} 
+    end 
+
+    def drivers
+        Driver.all.select { |driver| driver == self}
+    end
+
+    def total_distance
+    end
+
+
+    def self.all
+        @@all 
+    end
+
+    def self.premium_members
+        self.all.select { |premium| premium.total_distance > 100}
+    end
+
+
+end
+
+
+
+
+
+
+
+# - `Passenger#total_distance`
+#   - Returns the floating number that represents the total distance the passenger has travelled using the service
